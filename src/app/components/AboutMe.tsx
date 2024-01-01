d8779b5b09f3e5c7d92ea3ee3ab456bba3cd9391
nicocorradini <nicocorradini471@gmail.com>
2024-01-01 16:02:14 -0300

    feat: add AboutMe section

---
+++ src/app/components/AboutMe.tsx
@@ -0,0 +1,40 @@
+import EmailIcon from '@Icons/EmailIcon';
+import GitHubIcon from '@Icons/GitHubIcon';
+import LinkedInIcon from '@Icons/LinkedInIcon';
+import ButtonWithIcon from 'app/shared/ButtonWithIcon';
+
+function AboutMe() {
+  return (
+    <section className="mt-[150px] flex max-w-[570px] flex-col gap-[32px]">
+      <h1 className="text-platinum border-l-vistaBlue border-l-4 border-solid pl-4 text-[48px] font-semibold leading-none md:text-[60px]">
+        Corradini Nicolás
+      </h1>
+      <p className="text-platinum text-md font-light md:text-2xl">
+        Soy desarrollador web con{' '}
+        <span className="text-highlighted">+1 año de experiencia</span>. He
+        participado en diversos proyectos, donde he desarrollado{' '}
+        <span className="text-highlighted">UIs</span> y{' '}
+        <span className="text-highlighted">servicios REST</span>.
+      </p>
+      <div className="flex gap-4">
+        <ButtonWithIcon
+          text="LinkedIn"
+          icon={<LinkedInIcon className="h-5" />}
+          href="https://www.linkedin.com/in/nicolascorradini/"
+        />
+        <ButtonWithIcon
+          text="GitHub"
+          icon={<GitHubIcon className="h-5" />}
+          href="https://github.com/ncorradini"
+        />
+        <ButtonWithIcon
+          text="nicocorradini471@gmail.com"
+          icon={<EmailIcon className="h-5" />}
+          href="mailto:nicocorradini471@gmail.com"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import AboutMe from '@Components/AboutMe';
+
 export default function Home() {
-  return <main>Init project</main>;
+  return (
+    <main className="p-10">
+      <div className="absolute left-0 top-0 z-[-2] min-h-screen w-full bg-[#20252C] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(90,120,215,0.2),rgba(0,0,0,0.1))]" />
+      <div className="m-auto max-w-[900px]">
+        <AboutMe />
+      </div>
+    </main>
+  );
 }
