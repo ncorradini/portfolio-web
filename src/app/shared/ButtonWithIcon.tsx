bea5767d1bb1e6063418a8fa69a32df1b780d1f8
nicocorradini <nicocorradini471@gmail.com>
2024-01-06 12:20:11 -0300

    feat: improve ButtonWithIcon component

--- src/app/shared/ButtonWithIcon.tsx
+++ src/app/shared/ButtonWithIcon.tsx
@@ -1,29 +1,34 @@
 import { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type Props = {
   button: {
     text: string;
-    icon: ReactNode;
+    icon?: ReactNode;
     href: string;
   };
+  className?: string;
 };
 
-function ButtonWithIcon({ button }: Props) {
+function ButtonWithIcon({ button, className }: Props) {
+  const mergeClasses = twMerge(
+    'hover:border-lightAzure flex w-fit min-w-[70px] items-center justify-center gap-2 rounded-full border-2 border-solid border-onyx bg-white/5 px-4 py-2 text-center text-platinum duration-300 ease-in-out hover:scale-105',
+    className
+  );
   const { text, icon, href } = button;
 
   return (
     <button type="button">
-      <a
-        href={href}
-        target="_blank"
-        className="flex w-fit min-w-[70px] items-center justify-center gap-2 rounded-full border-2 border-solid border-[#3a3a41] bg-white/5 px-4 py-2 text-center text-platinum duration-300 ease-in-out hover:scale-105 hover:border-vistaBlue"
-        rel="noreferrer"
-      >
+      <a href={href} target="_blank" className={mergeClasses} rel="noreferrer">
         {icon}
-        <p className="hidden text-sm md:block">{text}</p>
+        <p className={`${icon && 'hidden'} text-sm md:block`}>{text}</p>
       </a>
     </button>
   );
 }
+
+ButtonWithIcon.defaultProps = {
+  className: ''
+};
 
 export default ButtonWithIcon;
