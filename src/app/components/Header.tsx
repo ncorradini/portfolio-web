f36ac587aaf6a94d1452c23445dd73c8949f8dc6
nicocorradini <nicocorradini471@gmail.com>
2024-01-20 00:59:53 -0300

    feat: general improvements

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -22,15 +22,15 @@ function Header() {
   };
 
   return (
-    <header className="sticky top-0 z-50 hidden h-[60px] w-full border-b border-solid border-silverSand bg-[#ffffff29] backdrop-blur md:block dark:border-onyx dark:bg-[#1014184a]">
-      <div className="container m-auto h-full">
-        <nav className="ml-auto flex h-full w-fit items-center gap-7 text-sm text-chineseBlack dark:text-platinum">
+    <header className="sticky top-0 z-50 h-[60px] w-full border-b border-solid border-silverSand bg-[#ffffff29] backdrop-blur dark:border-onyx dark:bg-[#1014184a]">
+      <div className="container m-auto flex h-full items-center">
+        <nav className="ml-auto flex h-full w-fit items-center gap-5 px-4 text-sm font-light text-chineseBlack md:px-0 dark:text-platinum/75">
           {LINK_SECTIONS.map((link) => (
             <a
               key={link.id}
               href={`#${link.id}`}
               onClick={handleNavigation}
-              className="transition duration-200 hover:text-darkSilver dark:hover:text-cadetBlue"
+              className="hidden transition duration-200 hover:text-darkSilver md:block dark:hover:text-white"
             >
               {link.text}
             </a>
--- src/app/shared/ButtonWithIcon.tsx
+++ src/app/shared/ButtonWithIcon.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-type Props = {
+type TButtonWithIconsProps = {
   button: {
     text: string;
     icon?: ReactNode;
@@ -10,7 +10,7 @@ type Props = {
   className?: string;
 };
 
-function ButtonWithIcon({ button, className = '' }: Props) {
+function ButtonWithIcon({ button, className = '' }: TButtonWithIconsProps) {
   const mergeClasses = twMerge(
     'hover:border-azure dark:hover:border-lightAzure flex w-fit min-w-[70px] items-center justify-center gap-2 rounded-full border-2 border-solid border-onyx/40 dark:border-onyx bg-white/5 px-4 py-2 text-center text-onyx/80 dark:text-platinum duration-300 hover:scale-105',
     className
